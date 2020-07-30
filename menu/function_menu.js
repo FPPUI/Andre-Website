aad055af426f6a557ff37372c15bbe681bb6bad7
Matheus Felipe <mathfelipe3011@gmail.com>
2020-07-30 04:26:24 +0000

    BackEnd do Menu Funcional

--- menu/function_menu.js
+++ menu/function_menu.js
@@ -1,27 +1,15 @@
-function incluirMenu(){
-    var elementosTag, contador, elemento, file, xml_http_req;
-    /* Loop com a coleção de todos os htmls */
-    elementosTag = document.getElementsByTagName("*");
-    for (contador = 0; contador < elementosTag.length; contador++) {
-        elemento = elementosTag[contador];
-        /* Procura por elementos com certos atributos*/
-        file = elemento.getAttribute("carregar-menu");
-        if (file) {
-            /* Faz uma requisição HTTP usando o atributo como nome do arquivo */
-            xml_http_req = new XMLHttpRequest();
-            xml_http_req.onreadystatechange = function() {
-                if (this.readyState == 4) {
-                    if (this.status == 200) {elemento.innerHTML = this.responseText;}
-                    if (this.status == 404) {elemento.innerHTML = "Page not found.";}
-                    /* Remove o atributo */
-                    elemento.removeAttribute("carregar-menu");
-                
-                }
-            }
-            xml_http_req.open("GET", file, true);
-            xml_http_req.send();
-            /* Encerra a função */
-            return;
+function incluirMenu(nomepag){
+    var contador;
+    var lista = ["Apostilas", "Projetos", "Atividades", "Programas", "Cursos", "Pesquisas", "Feira de Ciências"];
+
+    contador2 = 1; 
+    for(contador = 0; contador < lista.length; contador++){
+        if(nomepag !== lista[contador]){ 
+            document.getElementById("menu"+contador2).innerHTML = lista[contador];
+            contador2++;
+        } else {
+            document.getElementById("titulo").innerHTML = lista[contador];
         }
     }
 }
+
